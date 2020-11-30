25d5a4be6b2d4b0d7c29eb9d57f64090b53831a7
Aaron Vontell <vontell@mit.edu>
2020-11-30 09:48:33 -0500

    Foundation should be pip installable

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Foundation
 A library for defining types and interfaces to be used across Instabase apps and libraries.
+
+## Installing
+
+If installing from source, run the following command in this projects root directory
+
+```
+pip install .
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="instabase-foundation",
+    version="0.0.1",
+    author="Instabase",
+    author_email="support@instabase.com",
+    description="A library defining types and interfaces to be used across Instabase.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/instabase/foundation",
+    packages=['foundation'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_dir={'': 'py'},
+    install_requires=[
+        'mypy==0.790',
+        'mypy-extensions==0.4.3',
+        'mypy-protobuf==1.23',
+        'protobuf==3.14.0',
+        'six==1.15.0',
+        'typed-ast==1.4.1',
+        'typing-extensions==3.7.4.3',
+    ]
+)
