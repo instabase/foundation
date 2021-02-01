3df77ed7dc7998cfd2160a1ae7da17a492452307
ddanco <ddanco@gmail.com>
2021-01-31 22:49:54 -0800

    Break out entities in Document for JSON purposes

--- py/foundation/document.py
+++ py/foundation/document.py
@@ -2,9 +2,9 @@
 
 from dataclasses import dataclass, field
 from itertools import chain
-from typing import Tuple, Iterable, Type, TypeVar
+from typing import Dict, Tuple, Iterable, Type, TypeVar
 
-from .entity import Entity
+from .entity import *
 from .geometry import BBox
 from .typing_utils import unwrap
 
@@ -13,21 +13,41 @@ E = TypeVar('E', bound=Entity)
 
 @dataclass(frozen=True)
 class Document:
-  """A Foundation Document is a collection of Entity.
+  """A Foundation Document is a collection of Entities of varying type.
   """
   bbox: BBox
-  entities: Tuple[Entity, ...] = field(default_factory=tuple)
+  pages: Tuple[Page, ...] = field(default_factory=tuple)
+  words: Tuple[Word, ...] = field(default_factory=tuple)
+  phrases: Tuple[Phrase, ...] = field(default_factory=tuple)
+  paragraphs: Tuple[Paragraph, ...] = field(default_factory=tuple)
+  tables: Tuple[Table, ...] = field(default_factory=tuple)
+  numbers: Tuple[Number, ...] = field(default_factory=tuple)
+  integers: Tuple[Integer, ...] = field(default_factory=tuple)
+  dates: Tuple[Date, ...] = field(default_factory=tuple)
+  times: Tuple[Time, ...] = field(default_factory=tuple)
+  currencies: Tuple[Currency, ...] = field(default_factory=tuple)
+  person_names: Tuple[PersonName, ...] = field(default_factory=tuple)
+  addresses: Tuple[Address, ...] = field(default_factory=tuple)
+  clusters: Tuple[Cluster, ...] = field(default_factory=tuple)
+  named_entities: Tuple[NamedEntity, ...] = field(default_factory=tuple)
 
-  @staticmethod
-  def from_entities(entities: Iterable[Entity]) -> 'Document':
-    """Construct a Document with bbox from entities."""
-    entities = tuple(entities)
-    bbox = unwrap(BBox.union(e.bbox for e in entities))
-    return Document(bbox, entities)
+  @property
+  def entities(self) -> Tuple[Entity, ...]:
+    return tuple(chain(self.pages, self.words, self.phrases, self.paragraphs,
+                  self.tables, self.numbers, self.integers, self.dates,
+                  self.times, self.currencies, self.person_names,
+                  self.addresses, self.clusters, self.named_entities))
 
-  def with_entities(self, entities: Iterable[Entity]) -> 'Document':
-    """Returns a copy of this Document with given entities added. """
-    return Document.from_entities(tuple(chain(self.entities, entities)))
+  # @staticmethod
+  # def from_entities(entities: Iterable[Entity]) -> 'Document':
+  #   """Construct a Document with bbox from entities."""
+  #   entities = tuple(entities)
+  #   bbox = unwrap(BBox.union(e.bbox for e in entities))
+  #   return Document(bbox, entities)
+
+  # def with_entities(self, entities: Iterable[Entity]) -> 'Document':
+  #   """Returns a copy of this Document with given entities added. """
+  #   return Document.from_entities(tuple(chain(self.entities, entities)))
 
   def filter_entities(self, entity_type: Type[E]) -> Iterable[E]:
     yield from (e for e in self.entities if isinstance(e, entity_type))
