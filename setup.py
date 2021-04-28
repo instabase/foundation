0f2031140cb1fd8146bbcd401fc95d356da666ed
Erick Friis <erick@instabase.com>
2021-04-28 19:01:20 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="instabase-foundation",
-    version="0.0.3",
+    version="0.1.0",
     author="Instabase",
     author_email="support@instabase.com",
     description="Foundation",
