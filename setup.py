b5ca8e8ee3cf5fb041e48e8ec00ba44f88a558e7
Erick Friis <erick@instabase.com>
2021-04-28 16:32:42 -0400

    py 3.7 req'

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     package_dir={'': 'py'},
     install_requires=[
-        'mypy==0.790',
+        'mypy==0.701',
     ]
 )
