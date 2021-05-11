f525e308b821e8b3e362262d999e3a59db0e0018
Andrey Mishchenko <andrey@instabase.com>
2021-05-11 15:53:20 -0700

    Bump to Foundation v0.0.9

--- mp_packaging/mp_packaging.py
+++ mp_packaging/mp_packaging.py
@@ -22,7 +22,7 @@ def sanitize_for_path(s: str) -> str:
 
 TIMESTAMP = sanitize_for_path(str(datetime.datetime.now()))
 
-FOUNDATION_VERSION = '0.0.8'
+FOUNDATION_VERSION = '0.0.9'
 
 # Configure paths
 # ===============
--- ts/version.ts
+++ ts/version.ts
@@ -1,1 +1,1 @@
-export const FOUNDATION_VERSION = '0.0.8';
+export const FOUNDATION_VERSION = '0.0.9';
