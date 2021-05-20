49ab30e1b3fe5a9ba1bc5b92557af88d69d95027
Erick Friis <erick@instabase.com>
2021-05-19 20:59:09 -0400

    init

---
+++ py/foundation/__init__.py
@@ -0,0 +1,7 @@
+from foundation.comparison import ComparedValue, ComparedValueCollection
+from foundation.entity import Address, CurrencyAmount, Date, Entity, EntityCollection, FillerString, Page, PersonName, SubWord, Text, Word
+from foundation.extraction import ExtractedValue, ExtractedValueCollection
+from foundation.geometry import BBox, Interval, Point, Rectangle
+from foundation.record import RecordContext
+from foundation.serialization import Serialized, dumps, loads
+from foundation.targets import TargetValue, TargetValueCollection
